47452d6e79c9a7d1bbfef605c8405149e1e18421
Stefan Rehder <rehder@monsenso.com>
2018-06-07 22:48:52 +0200

    Correct tslint issues

--- src/pages/bitcoin/bitcoin.module.ts
+++ src/pages/bitcoin/bitcoin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { BitcoinPage } from './bitcoin';
 import { BitcoinProvider } from '../../providers/bitcoin/bitcoin';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
